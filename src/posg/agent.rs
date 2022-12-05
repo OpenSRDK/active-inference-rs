aa5aca849a19dc248e31a09c1d6e1a1f32ab939f
tibetsou <tibetsou@DESKTOP-ON4KGEA.localdomain>
2022-12-05 23:00:47 +0900

    feat: create stein(unfinished)

--- src/posg/agent.rs
+++ src/posg/agent.rs
@@ -1,9 +1,12 @@
-use crate::{Observation, PolicyOthers};
+use crate::{NonParametricPolicyOthers, Observation, PolicyOthers};
+use opensrdk_kernel_method::RBF;
 use opensrdk_probability::{
-    rand::{rngs::StdRng, RngCore, SeedableRng},
-    Distribution, DistributionError, RandomVariable, SampleableDistribution,
+    rand::{distributions::Uniform, rngs::StdRng, Rng, RngCore, SeedableRng},
+    stein::SteinVariational,
+    ConditionableDistribution, ContinuousSamplesDistribution, DiscreteUniform, Distribution,
+    DistributionError, InstantDistribution, RandomVariable, SampleableDistribution,
 };
-use std::marker::PhantomData;
+use std::{clone, marker::PhantomData};
 
 pub struct POSGAgent<S, Ai, AOthers, Oi, Obs, Ri, Pii, Thetai, PiOthers>
 where
@@ -11,13 +14,13 @@ where
     Ai: RandomVariable,
     AOthers: RandomVariable,
     Oi: RandomVariable,
-    Obs: Observation<S, Ai, AOthers, Oi>,
+    Obs: Observation<S, Ai, AOthers, Oi> + SampleableDistribution,
     Ri: Fn(&Ai, &S) -> f64,
     Pii: Distribution<Value = Ai, Condition = (S, Thetai)>,
     Thetai: RandomVariable,
-    PiOthers: PolicyOthers<S, AOthers>,
+    PiOthers: PolicyOthers<S, AOthers> + SampleableDistribution,
 {
-    inferred_state: S,
+    inferred_state: ContinuousSamplesDistribution<S>,
     observation: Obs,
     reward: Ri,
     policy: Pii,
@@ -29,18 +32,18 @@ where
 impl<S, Ai, AOthers, Oi, Obs, Ri, Pii, Thetai, PiOthers>
     POSGAgent<S, Ai, AOthers, Oi, Obs, Ri, Pii, Thetai, PiOthers>
 where
-    S: RandomVariable,
+    S: RandomVariable + std::cmp::PartialEq,
     Ai: RandomVariable,
     AOthers: RandomVariable,
     Oi: RandomVariable,
-    Obs: Observation<S, Ai, AOthers, Oi>,
-    Ri: Fn(&Ai, &S) -> f64,
+    Obs: Observation<S, Ai, AOthers, Oi> + SampleableDistribution,
+    Ri: Fn(&Ai, &S) -> f64 + std::marker::Sync,
     Pii: SampleableDistribution<Value = Ai, Condition = (S, Thetai)>,
     Thetai: RandomVariable,
-    PiOthers: PolicyOthers<S, AOthers>,
+    PiOthers: PolicyOthers<S, AOthers> + SampleableDistribution,
 {
     pub fn new(
-        inferred_state: S,
+        inferred_state: ContinuousSamplesDistribution<S>,
         observation: Obs,
         reward: Ri,
         policy: Pii,
@@ -59,20 +62,68 @@ where
     }
 
     pub fn sample_action(&self, rng: &mut dyn RngCore) -> Result<Ai, DistributionError> {
-        self.policy
-            .sample(&(self.inferred_state.clone(), self.theta.clone()), rng)
+        self.policy.sample(
+            &(self.inferred_state.mean().unwrap(), self.theta.clone()),
+            rng,
+        )
     }
 
     pub fn observe(&mut self, a_i: &Ai, o_i_next: &Oi) -> Result<f64, DistributionError> {
-        let previous_state = &self.inferred_state;
+        let previous_state_sample_distr = &self.inferred_state;
+        let previous_state = &previous_state_sample_distr.mean().unwrap();
         let mut rng = StdRng::from_seed([1; 32]);
-        let (inferred_s_next, inferred_a_others): (S, AOthers) =
-            todo!("{:#?}{:#?}{:#?}", self.observation, a_i, o_i_next); //(
-                                                                       //     previous_state.clone(),
-                                                                       //     self.policy_others.sample(pous_state, &mut rng).unwrap(),
-                                                                       // );
-                                                                       //self.observation, a_i, o_i_nextをつかって、inferred_s_next, inferred_a_othersを推定せよ
-                                                                       //observationとpolicy_othersのトレイトを使って推定する
+        //let (inferred_s_next, inferred_a_others): (S, AOthers) =
+        //    todo!("{:#?}{:#?}{:#?}", self.observation, a_i, o_i_next); //(
+        //     previous_state.clone(),
+        //     self.policy_others.sample(pous_state, &mut rng).unwrap(),
+        // );
+        //self.observation, a_i, o_i_nextをつかって、inferred_s_next, inferred_a_othersを推定せよ
+        //observationとpolicy_othersのトレイトを使って推定する
+        //スタイン
+        let value = vec![o_i_next];
+        let likelihood = InstantDistribution::new(
+            |o_i_next, &((_a, inferred_a_others), inferred_s_next)| {
+                self.observation.p_kernel(
+                    o_i_next,
+                    &((a_i.clone(), inferred_a_others), inferred_s_next),
+                )
+            },
+            |&((_a, inferred_a_others), inferred_s_next), rng| {
+                self.observation
+                    .sample(&((a_i.clone(), inferred_a_others), inferred_s_next), rng)
+            },
+        );
+        let prior_lhs = InstantDistribution::new(
+            |inferred_a_others, _s| {
+                self.policy_others
+                    .p_kernel(inferred_a_others, previous_state)
+            },
+            |_s, rng| self.policy_others.sample(previous_state, rng),
+        );
+        let prior_rhs = previous_state_sample_distr.clone();
+        let prior = prior_lhs * prior_rhs;
+        let kernel = RBF;
+        let kernel_params = [0.5, 0.5];
+        let samples_orig = (0..10)
+            .into_iter()
+            .map(|v| {
+                let mut rng3 = StdRng::from_seed([v; 32]);
+                let theta_0 = rng3.gen_range(-5.0..=5.0);
+                let mut rng4 = StdRng::from_seed([v * 2; 32]);
+                let theta_1 = rng4.gen_range(-5.0..=5.0);
+                vec![theta_0, theta_1]
+            })
+            .collect::<Vec<Vec<f64>>>();
+        let samples_theta = &mut ContinuousSamplesDistribution::new(samples_orig);
+
+        let mut stein = SteinVariational::new(
+            value,
+            &likelihood,
+            &prior,
+            &kernel,
+            &kernel_params,
+            samples_theta,
+        );
 
         let reward = (self.reward)(a_i, &inferred_s_next);
 
