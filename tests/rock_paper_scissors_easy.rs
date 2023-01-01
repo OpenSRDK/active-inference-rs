ea51e3e091ea3919ddc29f3b3f699c2f688e7f6e
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-01-01 17:03:59 +0900

    feat: easy2

--- tests/rock_paper_scissors_easy.rs
+++ tests/rock_paper_scissors_easy.rs
@@ -22,28 +22,24 @@ enum Hand {
 
 #[derive(Clone, Debug, PartialEq)]
 struct Data {
-    next_b1_sigma2_history: Vec<[f64; 2]>,
-    sigma1_history: Vec<[f64; 2]>,
     b1_sigma2_history: Vec<[f64; 2]>,
-    next_b2_sigma1_history: Vec<[f64; 2]>,
-    sigma2_history: Vec<[f64; 2]>,
+    sigma1_history: Vec<[f64; 2]>,
     b2_sigma1_history: Vec<[f64; 2]>,
+    sigma2_history: Vec<[f64; 2]>,
 }
 
 #[test]
 fn test_main() {
     let mut data = Data {
-        next_b1_sigma2_history: vec![],
-        sigma1_history: vec![],
         b1_sigma2_history: vec![],
-        next_b2_sigma1_history: vec![],
-        sigma2_history: vec![],
+        sigma1_history: vec![],
         b2_sigma1_history: vec![],
+        sigma2_history: vec![],
     };
     let mut sigma1 = [0.25, 0.25];
     let mut sigma2 = [0.25, 0.25];
-    let mut b1_sigma2 = [0.333333, 0.333333];
-    let mut b2_sigma1 = [0.333333, 0.333333];
+    let mut b1_sigma2 = [0.2, 0.2];
+    let mut b2_sigma1 = [0.2, 0.2];
     let mut t = 0usize;
     loop {
         let hand1 = random_hand(&sigma1);
@@ -51,14 +47,11 @@ fn test_main() {
         let next_b1_sigma2 = update_belief(t, hand2, &b1_sigma2);
         let next_b2_sigma1 = update_belief(t, hand1, &b2_sigma1);
 
-        learn2_by_1(&mut data, &next_b1_sigma2, &sigma1, &b1_sigma2);
-        learn1_by_2(&mut data, &next_b2_sigma1, &sigma2, &b2_sigma1);
+        learn2_by_1(&mut data, &next_b1_sigma2, &sigma1);
+        learn1_by_2(&mut data, &next_b2_sigma1, &sigma2);
 
-        expected_utility1(&sigma1, &next_b1_sigma2);
-        expected_utility2(&sigma2, &next_b2_sigma1);
-
-        sigma1 = optimize_policy1(&data, &sigma1, &b1_sigma2);
-        sigma2 = optimize_policy2(&data, &sigma2, &b2_sigma1);
+        sigma1 = optimize_policy1(&data);
+        sigma2 = optimize_policy2(&data);
 
         b1_sigma2 = next_b1_sigma2;
         b2_sigma1 = next_b2_sigma1;
@@ -101,75 +94,93 @@ fn update_belief(t: usize, others_hand: Hand, others_policy: &[f64; 2]) -> [f64;
     result
 }
 
-fn learn2_by_1(
-    data: &mut Data,
-    next_b1_sigma2: &[f64; 2],
-    sigma1: &[f64; 2],
-    b1_sigma2: &[f64; 2],
-) {
-    data.next_b1_sigma2_history.push(next_b1_sigma2.clone());
-    todo!()
-    //学習データ蓄える
+fn learn2_by_1(data: &mut Data, b1_sigma2: &[f64; 2], sigma1: &[f64; 2]) {
+    data.b1_sigma2_history.push(b1_sigma2.clone());
+    data.sigma1_history.push(sigma1.clone());
 }
 
-fn learn1_by_2(
-    data: &mut Data,
-    next_b2_sigma1: &[f64; 2],
-    sigma2: &[f64; 2],
-    b2_sigma1: &[f64; 2],
-) {
-    todo!()
+fn learn1_by_2(data: &mut Data, b2_sigma1: &[f64; 2], sigma2: &[f64; 2]) {
+    data.b2_sigma1_history.push(b2_sigma1.clone());
+    data.sigma2_history.push(sigma2.clone());
 }
 
-fn predict2_by_1(data: &Data, sigma1: &[f64; 2], b1_sigma2: &[f64; 2]) -> [f64; 2] {
-    //ガウス過程回帰
-    let y_rock = data
-        .next_b1_sigma2_history
+fn predict(
+    y_rock_prop_scissors: &[f64],
+    y_paper_prop_scissors: &[f64],
+    x: Vec<Vec<f64>>,
+    xs: Vec<f64>,
+) -> [f64; 2] {
+    let kernel = RBF + Periodic;
+    let theta = vec![1.0; kernel.params_len()];
+    let sigma = 1.0;
+
+    let params_rock_prop_scissors =
+        BaseEllipticalProcessParams::new(kernel.clone(), x.clone(), theta.clone(), sigma)
+            .unwrap()
+            .exact(&y_rock_prop_scissors)
+            .unwrap();
+
+    let result_rock_prop_scissors = params_rock_prop_scissors.gp_predict(&xs).unwrap();
+
+    let params_paper_prop_scissors = BaseEllipticalProcessParams::new(kernel, x, theta, sigma)
+        .unwrap()
+        .exact(&y_paper_prop_scissors)
+        .unwrap();
+
+    let result_paper_prop_scissors = params_paper_prop_scissors.gp_predict(&xs).unwrap();
+
+    let scissors =
+        1.0 / (1.0 / result_rock_prop_scissors.mu() + 1.0 / result_paper_prop_scissors.mu() + 1.0);
+    let rock = scissors / result_rock_prop_scissors.mu();
+    let paper = scissors / esult_paper_prop_scissors.mu();
+
+    [rock, paper]
+}
+
+fn predict2_by_1(data: &Data, sigma1: &[f64; 2]) -> [f64; 2] {
+    let y_rock_prop_scissors = data
+        .b1_sigma2_history
         .iter()
-        .map(|next_b1_sigma2| next_b1_sigma2[0])
+        .map(|b1_sigma2| (1.0 - b1_sigma2[0] - b1_sigma2[1]) / b1_sigma2[0])
         .collect::<Vec<f64>>();
-    let y_paper = data
-        .next_b1_sigma2_history
+    let y_paper_prop_scissors = data
+        .b1_sigma2_history
         .iter()
-        .map(|next_b1_sigma2| next_b1_sigma2[1])
+        .map(|b1_sigma2| (1.0 - b1_sigma2[0] - b1_sigma2[1]) / b1_sigma2[1])
         .collect::<Vec<f64>>();
 
     let x = data
         .sigma1_history
         .iter()
-        .zip(data.b1_sigma2_history.iter())
-        .map(|(sigma1, b1_sigma2)| [sigma1.to_vec(), b1_sigma2.to_vec()].concat())
+        .map(|(sigma1)| sigma1.to_vec())
         .collect::<Vec<_>>();
 
-    let kernel = RBF + Periodic;
-    let theta = vec![1.0; kernel.params_len()];
-    let sigma = 1.0;
+    let xs = sigma1.to_vec();
 
-    let xs = [sigma1.to_vec(), b1_sigma2.to_vec()].concat();
-    let params_rock =
-        BaseEllipticalProcessParams::new(kernel.clone(), x.clone(), theta.clone(), sigma)
-            .unwrap()
-            .exact(&y_rock)
-            .unwrap();
-
-    let np_rock = params_rock.gp_predict(&xs).unwrap();
-    let mu_rock = np_rock.mu();
-    let sigma_rock = np_rock.sigma();
-
-    let params_paper = BaseEllipticalProcessParams::new(kernel, x, theta, sigma)
-        .unwrap()
-        .exact(&y_paper)
-        .unwrap();
-
-    let np_paper = params_paper.gp_predict(&xs).unwrap();
-    let mu_paper = np_paper.mu();
-    let sigma_paper = np_paper.sigma();
-
-    [mu_rock, mu_paper]
+    predict(&y_rock_prop_scissors, &y_rock_prop_scissors, x, xs)
 }
 
-fn predict1_by_2(data: &Data, sigma2: &[f64; 2], b2_sigma1: &[f64; 2]) -> [f64; 2] {
-    todo!()
+fn predict1_by_2(data: &Data, sigma2: &[f64; 2]) -> [f64; 2] {
+    let y_rock_prop_scissors = data
+        .b2_sigma1_history
+        .iter()
+        .map(|b2_sigma1| (1.0 - b2_sigma1[0] - b2_sigma1[1]) / b2_sigma1[0])
+        .collect::<Vec<f64>>();
+    let y_paper_prop_scissors = data
+        .b2_sigma1_history
+        .iter()
+        .map(|b2_sigma1| (1.0 - b2_sigma1[0] - b2_sigma1[1]) / b2_sigma1[1])
+        .collect::<Vec<f64>>();
+
+    let x = data
+        .sigma2_history
+        .iter()
+        .map(|(sigma2)| sigma2.to_vec())
+        .collect::<Vec<_>>();
+
+    let xs = sigma2.to_vec();
+
+    predict(&y_rock_prop_scissors, &y_rock_prop_scissors, x, xs)
 }
 
 fn expected_utility1(sigma1: &[f64; 2], next_b1_sigma2: &[f64; 2]) -> f64 {
@@ -202,14 +213,26 @@ fn expected_utility2(sigma2: &[f64; 2], next_b2_sigma1: &[f64; 2]) -> f64 {
     utility2
 }
 
-fn optimize_policy1(data: &Data, sigma1: &[f64; 2], b1_sigma2: &[f64; 2]) -> [f64; 2] {
-    let predicted_2 = predict2_by_1(data, sigma1, b1_sigma2);
+fn best_response(others_policy: &[f64; 2]) -> [f64; 2] {
+    // https://tsujimotter.hatenablog.com/entry/the-proof-of-the-existence-of-nash-equilibria
+    let potential_improvement = || {};
     todo!()
-    //CMA-ES
 }
 
-fn optimize_policy2(data: &Data, sigma2: &[f64; 2], b2_sigma1: &[f64; 2]) -> [f64; 2] {
-    let predicted_1 = predict1_by_2(data, sigma2, b2_sigma1);
+fn best_response1(b1_sigma2: &[f64; 2]) -> [f64; 2] {
+    best_response(b1_sigma2)
+}
+
+fn best_response2(b2_sigma1: &[f64; 2]) -> [f64; 2] {
+    best_response(b2_sigma1)
+}
+
+fn optimize_policy1(data: &Data) -> [f64; 2] {
+    // let predicted_2 = predict2_by_1(data, sigma1, b1_sigma2);
     todo!()
-    //CMA-ES
+}
+
+fn optimize_policy2(data: &Data) -> [f64; 2] {
+    // let predicted_1 = predict1_by_2(data, sigma2, b2_sigma1);
+    todo!()
 }
